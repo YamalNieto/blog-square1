9d5f5f810a3b05f83db9760ef44b480662ebeec1
Yamal Nieto <yamalnieto@gmail.com>
2022-02-16 20:12:43 +0100

    Dockerized Laravel project with Breeze

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+}
